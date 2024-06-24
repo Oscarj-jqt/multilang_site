bc63314f68f0dbf28c95e4327d504608dca0698c
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-06-24 13:11:59 +0200

    final commit

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -15,6 +15,7 @@ import os
 from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
